944d71d220d253f0f83e6f2ad73ddf803e2e1360
Marcelo Lemes <marcelolemes@usp.br>
2023-06-13 21:09:23 -0300

    FIX uso optional no Service.buscarPorId()

--- src/main/java/com/devsuperior/desafio3/controllers/ClientController.java
+++ src/main/java/com/devsuperior/desafio3/controllers/ClientController.java
@@ -12,22 +12,28 @@ import org.springframework.web.bind.annotation.RestController;
 import com.devsuperior.desafio3.dto.ClientDTO;
 import com.devsuperior.desafio3.service.ClientService;
 
+import java.util.Optional;
+
 @RestController
 @RequestMapping(value = "/clients")
 public class ClientController {
 
-	@Autowired
-	private ClientService service;
-	
-	@GetMapping(value = "/{id}")
-	public ResponseEntity<ClientDTO> buscarPorId(@PathVariable Long id) {
-		ClientDTO dto = service.buscarPorId(id);
-		return ResponseEntity.ok(dto);
-	}
-	
-	@GetMapping
-	public ResponseEntity<Page<ClientDTO>> buscaPaginada(Pageable pageable){
-		Page<ClientDTO> dto = service.buscaPaginada(pageable);
-		return ResponseEntity.ok(dto);
-	}
+    @Autowired
+    private ClientService service;
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<ClientDTO> buscarPorId(@PathVariable Long id) {
+        Optional<ClientDTO> dto = service.buscarPorId(id);
+        if (dto.isPresent()) {
+            return ResponseEntity.ok(dto.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<ClientDTO>> buscaPaginada(Pageable pageable) {
+        Page<ClientDTO> dto = service.buscaPaginada(pageable);
+        return ResponseEntity.ok(dto);
+    }
 }
--- src/main/java/com/devsuperior/desafio3/service/ClientService.java
+++ src/main/java/com/devsuperior/desafio3/service/ClientService.java
@@ -15,22 +15,25 @@ import com.devsuperior.desafio3.repositories.ClientRepository;
 @Service
 public class ClientService {
 
-	@Autowired
-	private ClientRepository repository;
-	
-	@Transactional(readOnly = true)
-	public ClientDTO buscarPorId(Long id) {
-		Optional<Client> result = repository.findById(id);
-		Client client = result.get();
-		ClientDTO dto = new ClientDTO(client);
-		return dto;
-		
-	}
-	
-	@Transactional(readOnly = true)
-	public Page<ClientDTO> buscaPaginada(Pageable pageable) { 
-		Page<Client> result = repository.findAll(pageable);
-		return result.map(x -> new ClientDTO(x));
-	}
-	
+    @Autowired
+    private ClientRepository repository;
+
+    @Transactional(readOnly = true)
+    public Optional<ClientDTO> buscarPorId(Long id) {
+        Optional<Client> result = repository.findById(id);
+        Client client = result.orElseGet(()-> null);
+        Optional<ClientDTO> dto = Optional.empty();
+        if (client != null) {
+            dto = Optional.of(new ClientDTO(client));
+        }
+        return dto;
+
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ClientDTO> buscaPaginada(Pageable pageable) {
+        Page<Client> result = repository.findAll(pageable);
+        return result.map(x -> new ClientDTO(x));
+    }
+
 }
